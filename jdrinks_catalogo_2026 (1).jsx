42e0150826a7d676126c8071f171355c6ebd595d
joacoazambuya-lab <joacoazambuya@gmail.com>
2026-03-05 15:43:16 -0300

    Update jdrinks_catalogo_2026 (1).jsx

--- jdrinks_catalogo_2026 (1).jsx
+++ jdrinks_catalogo_2026 (1).jsx
@@ -14,13 +14,13 @@ const products = {
     { name: "Rojo + Coca 3L", price: "$990", img: "PEGAR_LINK_PNG_AQUI" }
   ],
   Whisky: [
-    { name: "Blue Label 750ml", price: "$10.500", img: "https://e7.pngegg.com/pngimages/718/1015/png-clipart-blended-whiskey-scotch-whisky-wine-distilled-beverage-wine-distilled-beverage-label-thumbnail.png" },
+    { name: "Blue Label 750ml", price: "$10.500", img: "https://thechamber.my/files/product_img/973/Johnnie-Walker-Blue-Label.png" },
     { name: "Green Label 750ml", price: "$3.500", img: "https://www.wineandbeyond.ca/cdn/shop/files/1002369_1.png?v=1745942779" },
     { name: "Gold Label 750ml", price: "$2.700", img: "https://licoresinmemorial.com/wp-content/uploads/2023/04/335641-800-auto.png" },
     { name: "Double Black 1L", price: "$2.100", img: "https://clink.ph/cdn/shop/products/WEB-johnnie-walker-double-black.png?v=1750646497" },
     { name: "Black Label 1L", price: "$1.650", img: "https://tadadelivery.com.uy/cdn/shop/files/whisky-johnnie-walker-black-label-750-ml-Photoroom.png?v=1748011504" },
     { name: "Red Label 1L", price: "$800", img: "https://cdn.awsli.com.br/800x800/2649/2649600/produto/234522026/jw-red-label---750ml--2xw11xihix.png" },
-    { name: "Buchanans Deluxe", price: "$1.900", img: "https://i.pinimg.com/736x/a9/ba/93/a9ba93d63276356ba5fe3d6f2a312f70.jpg" },
+    { name: "Buchanans Deluxe", price: "$1.900", img: "https://cdn.awsli.com.br/2500x2500/2649/2649600/produto/234522045/buchanan-s-deluxe-aged-12-years---1-0l-x-12---sc-h4p3cmcyws.png" },
     { name: "Chivas Regal 12 años 1L", price: "$1.650", img: "https://www.boozeit.com.au/cdn/shop/products/382072-1.png?v=1680681204" },
     { name: "Jack Daniels N7 1L", price: "$1.350", img: "https://desaonline.vtexassets.com/arquivos/ids/160621/jack-n7-1L_Mesa-de-trabajo-1.png?v=638291751625830000" },
     { name: "Jack Daniels Apple 1L", price: "$1.350", img: "https://cheers.com.np/uploads/products/76307263224386160728669066808496613407061.png" },
